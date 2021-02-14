640280f04d2465a56f7c49a7509218a28db265fd
Anartz Mugika Ledo <mugan86@gmail.com>
2021-02-14 19:24:12 +0100

    5-Implementado principios y configuraciones de la bola

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float _speed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity =
+            Vector2.right * _speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
