c28e8883261cce08f2d385ef519e6f5d24289ff9
Anartz Mugika Ledo <mugan86@gmail.com>
2021-02-14 17:46:17 +0100

    Sección 4 - Bases con las raquetas, físicas y movimientos OK

---
+++ Assets/Scripts/AddPoints.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AddPoints : MonoBehaviour
+{
+    [SerializeField] GameObject _pointGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 25; i++)
+        {
+            GameObject pointGameObject = Instantiate(_pointGameObject,
+                new Vector2(0, 36f - 3 * i), Quaternion.identity);
+            pointGameObject.transform.parent =
+                GameObject.Find("CenterPoints").transform;
+        }
+    }
+}
