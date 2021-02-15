277b769967eb6d7912ab652f29b6ab495f7337a9
Anartz Mugika Ledo <mugan86@gmail.com>
2021-02-15 18:55:12 +0100

    Secciones 6 / 7 Colisiones + Porteria OK

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,9 +10,43 @@ public class Ball : MonoBehaviour
             Vector2.right * _speed;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        /*
+            collision: Información del elemento con el que colisiona.
+            Por ejemplo la raqueta, pared,...
+            collision.gameObject = Elemento con el que chocamos
+            collision.gameObject.tag = Tag del elemento con el que chocamos
+            collision.gameObject.name = Nombre del elemento con el que chocamos
+            Para la pelota hacemos uso del gameObject
+         */
+        if (collision.gameObject.tag == "Racket")
+        {
+            // Obtenemos el valor del factor de impacto vertical
+            float y = HitVerticalFactor(gameObject.transform.position.y,
+            collision.gameObject.transform.position.y,
+            collision.collider.bounds.size.y);
+
+            // Obtener la dirección horizontal dependiendo en que raqueta impacta
+            float x = (collision.gameObject.name == "Left") ? 1 : -1;
+
+            Vector2 direction = new Vector2(x, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = direction * _speed;
+        } else
+        {
+            Debug.Log("Colisiona con otro elemento");
+        }
+
         
+    }
+
+    float HitVerticalFactor(
+        float ballPosition,
+        float racketPosition,
+        float racketHeight)
+    {
+        return (ballPosition - racketPosition) / racketHeight;
     }
 }
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.name == "Left")
+        {
+            Debug.Log("Izquierda recibe gol. Derecha marca gol");
+        } else if (gameObject.name == "Right")
+        {
+            Debug.Log("Derecha recibe gol. Izquierda marca gol");
+        }
+    }
+}
