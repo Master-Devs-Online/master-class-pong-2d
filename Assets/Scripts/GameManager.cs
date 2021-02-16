802efad15f8afb20454e515a10ea6fd998bad904
Anartz Mugika Ledo <mugan86@gmail.com>
2021-02-16 17:13:59 +0100

    Sección 8 - Gestión del marcador con GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] byte[] _scoreBoard = new byte[2];
+    public byte [] ScoreBoard
+    {
+        get => _scoreBoard;
+        set
+        {
+            _scoreBoard = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scoreBoard[0] = 0;
+        _scoreBoard[1] = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,14 +2,22 @@
 
 public class Goal : MonoBehaviour
 {
+    private GameManager _gameManager;
+
+    void Start()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (gameObject.name == "Left")
         {
             Debug.Log("Izquierda recibe gol. Derecha marca gol");
+            _gameManager.ScoreBoard[1] += 1;
         } else if (gameObject.name == "Right")
         {
             Debug.Log("Derecha recibe gol. Izquierda marca gol");
+            _gameManager.ScoreBoard[0] += 1;
         }
     }
 }
